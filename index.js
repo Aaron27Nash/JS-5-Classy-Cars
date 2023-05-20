f898cbf9cf85905d8aa6de4ff20ffd3f9a96bfe6
Aaron Nash <aaron27nash@gmail.com>
2023-05-20 09:48:47 -0400

    final upload

---
+++ index.js
@@ -0,0 +1,39 @@
+class Vehicle{
+    constructor (model){
+        this.model = model
+    }
+    drive(){
+        return this.model + " " + "drives"
+    }
+} 
+class Sedan extends Vehicle {
+    constructor (model, backupCamera){
+       super (model) 
+       this.backupCamera = backupCamera;
+    }
+
+        drive(){
+            return `$wq{this.model} "drives";
+        }
+}
+class Suv extends Vehicle{
+    constructor (model, offroadPackage){
+        super (model);
+        this.model = model;
+        this.offroadPackage = offroadPackage;
+}
+    drive(){
+        return this.model + " " "drives";
+    }
+}
+class Truck extends Vehicle{
+    constructor (model, towingCapacity){
+        super (model)
+        this.model = model
+        this.towingPackage = towingPackage
+
+    }
+    drive(){
+        return this.model + " " "drives"
+    }
+}
